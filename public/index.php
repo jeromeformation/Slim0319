d3e9f6cef6ca193b0b030c8ea4c1190702c92225
jerome <ambroise.jerome.formation@gmail.com>
2019-03-25 16:37:57 +0100

    Routage : variable dans l URL et groupe de route

--- public/index.php
+++ public/index.php
@@ -11,14 +11,30 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = new App();
 
 // Création d'une route
-$app->get("/hello", function (
-    ServerRequestInterface $request,
-    ResponseInterface $response,
-    ?array $args
-) {
-        // On retourne une réponse
-        return $response->getBody()->write('<h1>Bonjour</h1>');
+$route = $app->get("/", function (ServerRequestInterface $request, ResponseInterface $response, ?array $args) {
+    // On retourne une réponse
+    return $response->getBody()->write('<h1>Bonjour</h1>');
 });
+$route->setName('homepage');
+
+$app->group('/projet', function() {
+    // Création d'une page de détail des projets
+    // Nouveauté : on ajoute une variable dans l'URL avec les accolades
+    $this->get("/{id:\d+}", function (ServerRequestInterface $request, ResponseInterface $response, ?array $args) {
+        // On retourne une réponse
+        return $response->getBody()->write('<h1>Détail du projet</h1>');
+    })->setName('app_project_show');
+
+    // Page de création
+    $this->get("/creation", function (ServerRequestInterface $request, ResponseInterface $response, ?array $args) {
+        // On retourne une réponse
+        return $response->getBody()->write('<h1>Création d\'un projet</h1>');
+    })->setName('app_project_create');
+});
+
+
+
+
 
 // Création et renvoi de la réponse au navigateur
 $app->run();
