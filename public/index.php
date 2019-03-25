a1b7f222dfeb1e3cfb07c97c5c12e10a61d26436
jerome <ambroise.jerome.formation@gmail.com>
2019-03-25 14:52:27 +0100

    htaccess - php code sniffeur - creation de la premiere page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "Bonjour";
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+// Autoloader
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// Création de l'application
+$app = new App();
+
+// Création d'une route
+$app->get("/hello", function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    ?array $args
+) {
+        // On retourne une réponse
+        return $response->getBody()->write('<h1>Bonjour</h1>');
+});
+
+// Création et renvoi de la réponse au navigateur
+$app->run();
