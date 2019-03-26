2a31f117d856ba04918797462f48a6034badd568
jerome <ambroise.jerome.formation@gmail.com>
2019-03-26 14:57:38 +0100

    Integration de TWIG - utilisation de TWIG dans ProjectController

--- public/index.php
+++ public/index.php
@@ -10,12 +10,42 @@ use Slim\App;
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // Création de l'application
-$app = new App();
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true
+    ]
+];
+$app = new App($config);
+
+// Configuration de TWIG
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(dirname(__DIR__) . '/templates', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
+$container[ProjectController::class] = function ($container) {
+    return new ProjectController($container->get('view'));
+};
+
 
 // Création d'une route
 $route = $app->get("/", function (ServerRequestInterface $request, ResponseInterface $response, ?array $args) {
     // On retourne une réponse
-    return $response->getBody()->write('<h1>Bonjour</h1>');
+    //return $response->getBody()->write('<h1>Bonjour</h1>');
+
+    return $this->view->render($response, 'home.twig');
 });
 $route->setName('homepage');
 
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,13 +3,25 @@ namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
 
 class ProjectController
 {
+    /**
+     * @var Twig
+     */
+    private $twig;
+
+    public function __construct(Twig $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public function show(ServerRequestInterface $request, ResponseInterface $response, ?array $args)
     {
         // On retourne une réponse
-        return $response->getBody()->write('<h1>Détail du projet</h1>');
+        //return $response->getBody()->write('<h1>Détail du projet</h1>');
+        return $this->twig->render($response, 'project/show.twig');
     }
 
     public function create(ServerRequestInterface $request, ResponseInterface $response, ?array $args)
